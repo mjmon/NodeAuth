86b14846367b509d0c4b8568112d388538c8d570
mjmon1995 <mjmon1995@gmail.com>
2019-11-30 18:21:16 +0800

    added middlewares

--- server/app.js
+++ server/app.js
@@ -3,11 +3,14 @@ const morgan = require('morgan')
 const uuid = require('uuid/v4')
 const session = require('express-session')
 const FileStore = require('session-file-store')(session)
+const bodyParser = require('body-parser')
 
 const app = express()
 
+//add & configure middlewares
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 app.use(morgan('dev'))
-
 app.use(session({
     genid: (req) => {
       console.log('Inside the session middleware')
@@ -23,11 +26,25 @@ app.use(session({
     saveUninitialized: true
 }))
 
+//homepage route
 app.get('/', (req, res) => {
     console.log('Inside the homepage callback function')
     console.log(req.sessionID)
     res.send(`You hit home page!\n`)
 })
+
+app.get('/login', (req, res) => {
+  console.log('Inside GET /login callback function')
+  console.log(req.sessionID)
+  res.send('You got the login page!\n')
+})
+
+app.post('/login', (req, res) => {
+  console.log('Inside POST /login callback function')
+  console.log(req.body)
+  res.send('You posted to the login page!\n')
+})
+
 
 app.listen(3000, () => {
     console.log('Listening on Port: 3000')
